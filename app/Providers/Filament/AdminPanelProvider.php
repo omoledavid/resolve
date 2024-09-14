25804e8ffcfe746918a33cfff7f73d3bd66d0f2c
Your Name <you@example.com>
2024-09-15 00:42:52 +0100

    fixes for link

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,8 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->profile()
+            ->passwordReset()
             ->colors([
                 'primary' => Color::Amber,
             ])
